a1e2f3579e39e7b7b16f6b383ed0e46c5187632f
joyce.alves <joycequintino11@gmail.com>
2024-01-16 01:52:47 -0300

    feat: structure organization and home page

---
+++ capoeira_app/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+
+function Header(){
+    return (
+        <header className={styles.header}>
+            <span>Jogos do Nordeste 2024 - Abadá Capoeira</span>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
