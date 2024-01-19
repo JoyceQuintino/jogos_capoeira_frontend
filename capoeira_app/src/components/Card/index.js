5bead17851febc3b5f1d796a8466d43f8bd80ccd
joyce.alves <joycequintino11@gmail.com>
2024-01-19 01:30:22 -0300

    reafctor: component Card

--- capoeira_app/src/components/Card/index.js
+++ capoeira_app/src/components/Card/index.js
@@ -3,6 +3,7 @@ import styles from './Card.module.css';
 function Card({ id }) {
     return(
         <section className={styles.card}>
+            <h2>Jogo</h2>
             {id}
         </section>
     );
